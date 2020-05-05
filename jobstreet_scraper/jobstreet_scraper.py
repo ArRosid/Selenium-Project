c8b836b28ab11ef3ea15def636334004e5be5ec0
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-05 20:39:57 +0700

    Scrape programmer jobs first page

--- jobstreet_scraper/jobstreet_scraper.py
+++ jobstreet_scraper/jobstreet_scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 import time
 import os
 
@@ -26,7 +27,28 @@ browser.find_element_by_id('login_btn').click()
 # search for programmer jobs
 browser.find_element_by_id('search_box_keyword').send_keys('programmer')
 browser.find_element_by_id('header_searchbox_btn').click()
+time.sleep(5)
 
+# sort by date
+select = Select(browser.find_element_by_id('sort_result'))
+select.select_by_visible_text('Date')
+time.sleep(5)
 
-time.sleep(10)
+# get all jobs
+all_jobs = browser.find_elements_by_xpath('//div[@class="panel "]')
+for job in all_jobs:
+    title = job.find_element_by_xpath('.//a[@class="position-title-link"]').text
+    try:
+        company = job.find_element_by_xpath('.//a[@class="company-name"]/span').text
+    except:
+        company = "Company Confidential"
 
+    location = job.find_element_by_xpath('.//li[@class="job-location"]/span').text
+    salary = job.find_element_by_xpath('.//li[@id="job_salary"]/font').text
+
+    print({
+        'title': title,
+        'company': company,
+        'location': location,
+        'salary': salary
+    })
